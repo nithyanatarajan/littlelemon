72f1763f494cea010c0838b1f65519e463920a12
Nithya Natarajan <nithyanatarajn@gmail.com>
2023-06-14 12:44:10 +0530

    Fix Reserve a table button feature

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,40 @@
 import React from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 import FoodImg from '../assets/restaurantfood.jpg';
 import './Home.css';
 
-const Home = () => (
-  <main>
-    <section className='hero'>
-      <div className='hero-section'>
-        <div className='hero-content'>
-          <h1>Little Lemon</h1>
-          <h2>Chicago</h2>
-          <p>
-            We are a family owned Mediterranean restaurant, focused on
-            traditional recipes served with a modern twist.
-          </p>
-          <a href='#reserve' role='button' className='primary-button'>
-            Reserve a table
-          </a>
+const Home = () => {
+  const navigate = useNavigate();
+
+  const handleReservationClick = () => {
+    navigate('/reservations');
+  };
+  return (
+    <main>
+      <section className='hero'>
+        <div className='hero-section'>
+          <div className='hero-content'>
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p>
+              We are a family owned Mediterranean restaurant, focused on
+              traditional recipes served with a modern twist.
+            </p>
+            <NavLink
+              to='/reservations'
+              role='button'
+              className='primary-button'
+              onClick={handleReservationClick}
+            >
+              Reserve a Table
+            </NavLink>
+          </div>
+          <div className='hero-image'>
+            <img src={FoodImg} alt='Restaurant Food' />
+          </div>
         </div>
-        <div className='hero-image'>
-          <img src={FoodImg} alt='Restaurant Food' />
-        </div>
-      </div>
-    </section>
-  </main>
-);
+      </section>
+    </main>
+  );
+};
 export default Home;
