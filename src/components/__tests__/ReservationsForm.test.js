85797209ea45a387f4a5696e80b8557ddc4e5e73
Nithya Natarajan <nithyanatarajn@gmail.com>
2023-06-20 20:31:47 +0530

    Refactor ReservationsForm tests - groups tests logically

--- src/components/__tests__/ReservationsForm.test.js
+++ src/components/__tests__/ReservationsForm.test.js
@@ -4,92 +4,88 @@ import userEvent from '@testing-library/user-event';
 import ReservationsForm from '../ReservationsForm';
 
 describe('ReservationsForm', () => {
-  test('should render the form with fields and a submit button', () => {
-    render(<ReservationsForm onSubmit={() => {}} />);
+  describe('render form fields', () => {
+    test('should render the form with fields and a submit button', () => {
+      render(<ReservationsForm onSubmit={() => {}} />);
 
-    const firstNameInput = screen.getByLabelText('First Name');
-    const lastNameInput = screen.getByLabelText('Last Name');
-    const button = screen.getByRole('button', { name: 'Reserve a Table' });
+      const firstNameInput = screen.getByLabelText('First Name');
+      const lastNameInput = screen.getByLabelText('Last Name');
+      const button = screen.getByRole('button', { name: 'Reserve a Table' });
 
-    expect(firstNameInput).toBeInTheDocument();
-    expect(lastNameInput).toBeInTheDocument();
-    expect(button).toBeInTheDocument();
-    expect(button).toBeDisabled();
-  });
-
-  test('should disable button when only firstname is given', async () => {
-    const handleSubmit = jest.fn();
-    render(<ReservationsForm onSubmit={handleSubmit} />);
-    const user = userEvent.setup();
-
-    const firstNameInput = screen.getByLabelText('First Name');
-    const button = screen.getByRole('button', { name: 'Reserve a Table' });
-
-    await user.type(firstNameInput, 'John');
-
-    expect(button).toBeDisabled();
-  });
-
-  test('should show error message when firstname is empty and focus moves out', async () => {
-    const handleSubmit = jest.fn();
-    render(<ReservationsForm onSubmit={handleSubmit} />);
-
-    const firstNameInput = screen.getByLabelText('First Name');
-    const button = screen.getByRole('button', { name: 'Reserve a Table' });
-    await firstNameInput.focus();
-    await act(() => {
-      firstNameInput.blur();
+      expect(firstNameInput).toBeInTheDocument();
+      expect(lastNameInput).toBeInTheDocument();
+      expect(button).toBeInTheDocument();
+      expect(button).toBeDisabled();
     });
-
-    await waitFor(() => {
-      expect(screen.getByText(/First Name is required/i)).toBeInTheDocument();
-    });
-    expect(button).toBeDisabled();
   });
+  describe('invalid form', () => {
+    test('should disable button when only firstname is given', async () => {
+      const handleSubmit = jest.fn();
+      render(<ReservationsForm onSubmit={handleSubmit} />);
+      const user = userEvent.setup();
 
-  test('should show error message when lastname is empty and focus moves out', async () => {
-    const handleSubmit = jest.fn();
-    render(<ReservationsForm onSubmit={handleSubmit} />);
+      const firstNameInput = screen.getByLabelText('First Name');
+      const button = screen.getByRole('button', { name: 'Reserve a Table' });
 
-    const lastNameInput = screen.getByLabelText('Last Name');
-    const button = screen.getByRole('button', { name: 'Reserve a Table' });
-    await lastNameInput.focus();
-    await act(() => {
-      lastNameInput.blur();
+      await user.type(firstNameInput, 'John');
+
+      expect(button).toBeDisabled();
     });
+    const testErrorMessageOnBlur = async (inputLabel, errorMessage) => {
+      const handleSubmit = jest.fn();
+      render(<ReservationsForm onSubmit={handleSubmit} />);
 
-    await waitFor(() => {
-      expect(screen.getByText(/Last Name is required/i)).toBeInTheDocument();
-    });
-    expect(button).toBeDisabled();
-  });
+      const input = screen.getByLabelText(inputLabel);
+      const button = screen.getByRole('button', { name: 'Reserve a Table' });
 
-  test('should submit the form with valid data and clear on submission', async () => {
-    const handleSubmit = jest.fn();
-    render(<ReservationsForm onSubmit={handleSubmit} />);
-    const user = userEvent.setup();
-
-    const firstNameInput = screen.getByLabelText('First Name');
-    const lastNameInput = screen.getByLabelText('Last Name');
-    const button = screen.getByRole('button', { name: 'Reserve a Table' });
-
-    await user.type(firstNameInput, 'John');
-    await user.type(lastNameInput, 'Dee');
-
-    expect(button).not.toBeDisabled();
-
-    await user.click(button);
-
-    await waitFor(() => {
-      expect(handleSubmit).toHaveBeenCalledWith({
-        firstName: 'John',
-        lastName: 'Dee',
+      await input.focus();
+      await act(() => {
+        input.blur();
       });
+
+      await waitFor(() => {
+        expect(screen.getByText(errorMessage)).toBeInTheDocument();
+      });
+      expect(button).toBeDisabled();
+    };
+
+    test('should show error message when firstname is empty and focus moves out', async () => {
+      await testErrorMessageOnBlur('First Name', /First Name is required/i);
     });
 
-    await waitFor(() => {
-      expect(firstNameInput.value).toBe('');
-      expect(lastNameInput.value).toBe('');
+    test('should show error message when lastname is empty and focus moves out', async () => {
+      await testErrorMessageOnBlur('Last Name', /Last Name is required/i);
+    });
+  });
+
+  describe('valid form', () => {
+    test('should submit the form with valid data and clear on submission', async () => {
+      const handleSubmit = jest.fn();
+      render(<ReservationsForm onSubmit={handleSubmit} />);
+      const user = userEvent.setup();
+
+      const firstNameInput = screen.getByLabelText('First Name');
+      const lastNameInput = screen.getByLabelText('Last Name');
+      const button = screen.getByRole('button', { name: 'Reserve a Table' });
+
+      await user.type(firstNameInput, 'John');
+      await user.type(lastNameInput, 'Dee');
+
+      expect(button).not.toBeDisabled();
+
+      await user.click(button);
+
+      await waitFor(() => {
+        expect(handleSubmit).toHaveBeenCalledWith({
+          firstName: 'John',
+          lastName: 'Dee',
+        });
+      });
+
+      await waitFor(() => {
+        expect(firstNameInput.value).toBe('');
+        expect(lastNameInput.value).toBe('');
+      });
     });
   });
 });
