9d68a806bb4a3f43d49f7b60da72fb2fdb47d135
Nithya Natarajan <nithyanatarajn@gmail.com>
2023-06-20 20:22:10 +0530

    Use formik in ReservationForm

--- src/components/ReservationsForm.js
+++ src/components/ReservationsForm.js
@@ -1,27 +1,81 @@
 import PropTypes from 'prop-types';
 import './ReservationsForm.css';
+import { useFormik } from 'formik';
 
 const ReservationsForm = ({ onSubmit }) => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSubmit({});
+  const initialValues = {
+    firstName: '',
+    lastName: '',
   };
 
+  const handleSubmit = (values, { resetForm }) => {
+    onSubmit(values);
+    resetForm();
+  };
+
+  const validateForm = (values) => {
+    const errors = {};
+
+    if (!values.firstName) {
+      errors.firstName = 'First Name is required';
+    }
+
+    if (!values.lastName) {
+      errors.lastName = 'Last Name is required';
+    }
+
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit: handleSubmit,
+    validate: validateForm,
+  });
+
   return (
-    <form className='form' onSubmit={handleSubmit}>
+    <form className='form' onSubmit={formik.handleSubmit}>
       <div className='form-row'>
         {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
         <label htmlFor='firstName'>First Name</label>
-        <input type='text' id='firstName' name='firstName' required />
-      </div>
+        <div className='form-input'>
+          <input
+            type='text'
+            id='firstName'
+            name='firstName'
+            /* eslint-disable-next-line react/jsx-props-no-spreading */
+            {...formik.getFieldProps('firstName')}
+            required
+          />
 
+          {formik.touched.firstName && formik.errors.firstName && (
+            <div className='form-error'>{formik.errors.firstName}</div>
+          )}
+        </div>
+      </div>
       <div className='form-row'>
         {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
         <label htmlFor='lastName'>Last Name</label>
-        <input type='text' id='lastName' name='lastName' required />
+        <div className='form-input'>
+          <input
+            type='text'
+            id='lastName'
+            name='lastName'
+            /* eslint-disable-next-line react/jsx-props-no-spreading */
+            {...formik.getFieldProps('lastName')}
+            required
+          />
+          {formik.touched.lastName && formik.errors.lastName && (
+            <div className='form-error'>{formik.errors.lastName}</div>
+          )}
+        </div>
       </div>
 
-      <button className='primary-button' type='submit'>
+      <button
+        className='primary-button'
+        type='submit'
+        disabled={!formik.isValid || formik.isSubmitting || !formik.dirty}
+      >
         Reserve a Table
       </button>
     </form>
--- src/components/__tests__/ReservationsForm.test.js
+++ src/components/__tests__/ReservationsForm.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import ReservationsForm from '../ReservationsForm';
 
 describe('ReservationsForm', () => {
@@ -13,5 +14,82 @@ describe('ReservationsForm', () => {
     expect(firstNameInput).toBeInTheDocument();
     expect(lastNameInput).toBeInTheDocument();
     expect(button).toBeInTheDocument();
+    expect(button).toBeDisabled();
+  });
+
+  test('should disable button when only firstname is given', async () => {
+    const handleSubmit = jest.fn();
+    render(<ReservationsForm onSubmit={handleSubmit} />);
+    const user = userEvent.setup();
+
+    const firstNameInput = screen.getByLabelText('First Name');
+    const button = screen.getByRole('button', { name: 'Reserve a Table' });
+
+    await user.type(firstNameInput, 'John');
+
+    expect(button).toBeDisabled();
+  });
+
+  test('should show error message when firstname is empty and focus moves out', async () => {
+    const handleSubmit = jest.fn();
+    render(<ReservationsForm onSubmit={handleSubmit} />);
+
+    const firstNameInput = screen.getByLabelText('First Name');
+    const button = screen.getByRole('button', { name: 'Reserve a Table' });
+    await firstNameInput.focus();
+    await act(() => {
+      firstNameInput.blur();
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText(/First Name is required/i)).toBeInTheDocument();
+    });
+    expect(button).toBeDisabled();
+  });
+
+  test('should show error message when lastname is empty and focus moves out', async () => {
+    const handleSubmit = jest.fn();
+    render(<ReservationsForm onSubmit={handleSubmit} />);
+
+    const lastNameInput = screen.getByLabelText('Last Name');
+    const button = screen.getByRole('button', { name: 'Reserve a Table' });
+    await lastNameInput.focus();
+    await act(() => {
+      lastNameInput.blur();
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText(/Last Name is required/i)).toBeInTheDocument();
+    });
+    expect(button).toBeDisabled();
+  });
+
+  test('should submit the form with valid data and clear on submission', async () => {
+    const handleSubmit = jest.fn();
+    render(<ReservationsForm onSubmit={handleSubmit} />);
+    const user = userEvent.setup();
+
+    const firstNameInput = screen.getByLabelText('First Name');
+    const lastNameInput = screen.getByLabelText('Last Name');
+    const button = screen.getByRole('button', { name: 'Reserve a Table' });
+
+    await user.type(firstNameInput, 'John');
+    await user.type(lastNameInput, 'Dee');
+
+    expect(button).not.toBeDisabled();
+
+    await user.click(button);
+
+    await waitFor(() => {
+      expect(handleSubmit).toHaveBeenCalledWith({
+        firstName: 'John',
+        lastName: 'Dee',
+      });
+    });
+
+    await waitFor(() => {
+      expect(firstNameInput.value).toBe('');
+      expect(lastNameInput.value).toBe('');
+    });
   });
 });
