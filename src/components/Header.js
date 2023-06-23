368d78226c5483e7a9d4f906a6f901bb0dd6389d
Nithya Natarajan <nithyanatarajn@gmail.com>
2023-06-24 00:51:57 +0530

    Make Logo in header to redirect to homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,33 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Nav from './Nav';
 import Logo from '../assets/headerlogo.png';
 import Hamburger from './Hamburger';
 import './Header.css';
 
-const Header = () => (
-  <header className='header'>
-    <Hamburger />
-    <div className='logo'>
-      <img
-        src={Logo}
-        alt='Little Lemon Restaurant Logo'
-        className='header-img'
-      />
-    </div>
-    <Nav />
-  </header>
-);
+const Header = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/');
+  };
+  return (
+    <header className='header'>
+      <Hamburger />
+      {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
+      <div
+        className='logo'
+        onClick={handleClick}
+        onKeyDown={handleClick}
+        role='img'
+      >
+        <img
+          src={Logo}
+          alt='Little Lemon Restaurant Logo'
+          className='header-img'
+        />
+      </div>
+      <Nav />
+    </header>
+  );
+};
 export default Header;
