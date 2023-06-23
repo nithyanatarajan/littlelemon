a5ac193805780339c1776f5af8ee3b8c800c6845
Nithya Natarajan <nithyanatarajn@gmail.com>
2023-06-23 12:10:48 +0530

    Style Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,17 @@ import './Footer.css';
 
 const Footer = () => (
   <footer className='footer'>
-    <div className='logo'>
-      <img
-        src={Logo}
-        alt='Little Lemon Restaurant Footer Logo'
-        className='footer-img'
-      />
-    </div>
-    <div className='copyright'>
-      <p>©2023 Little Lemon. All rights reserved. </p>
+    <div className='footer-section'>
+      <div className='logo'>
+        <img
+          src={Logo}
+          alt='Little Lemon Restaurant Footer Logo'
+          className='footer-img'
+        />
+      </div>
+      <div className='copyright'>
+        <p>©2023 Little Lemon. All rights reserved. </p>
+      </div>
     </div>
   </footer>
 );
