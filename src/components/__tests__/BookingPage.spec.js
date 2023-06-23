bdb119e5ea263a1ff59b0118f002188d2a35932c
Nithya Natarajan <nithyanatarajn@gmail.com>
2023-06-23 18:23:17 +0530

    Enhance BookingPage.spec.js

--- src/components/__tests__/BookingPage.spec.js
+++ src/components/__tests__/BookingPage.spec.js
@@ -5,34 +5,74 @@ import userEvent from '@testing-library/user-event';
 import BookingPage from '../BookingPage';
 import { fetchAPI } from '../../assets/api';
 
-jest.mock('../../assets/api');
-
-function setupStubs() {
-  jest.spyOn(console, 'log').mockImplementation(() => {});
-  fetchAPI.mockImplementation((date) => {
-    const dateStr = date.toISOString().substring(0, 10);
-    if (dateStr === '2023-06-20') {
-      return ['18:00', '18:30'];
-    }
-    if (dateStr === '2023-06-21') {
-      return ['17:00', '18:30'];
-    }
-    return ['19:00'];
-  });
-}
-
-function resetStubs() {
-  fetchAPI.mockReset();
-  console.log.mockRestore();
-}
+jest.mock('../../assets/api', () => ({
+  fetchAPI: jest.fn(),
+}));
 
 describe('BookingPage', () => {
+  beforeEach(() => {
+    jest.spyOn(console, 'log').mockImplementation();
+  });
+
   afterEach(() => {
-    resetStubs();
+    console.log.mockRestore();
+    jest.clearAllMocks();
+  });
+
+  test('should submit Booking Form', async () => {
+    const selectedDate = '2023-06-20';
+    fetchAPI.mockImplementation(() => ['18:00', '19:00']);
+
+    render(<BookingPage />);
+
+    const user = userEvent.setup();
+
+    const firstNameInput = screen.getByLabelText('First Name');
+    const lastNameInput = screen.getByLabelText('Last Name');
+    const dateInput = screen.getByLabelText('Choose date');
+    const timeInput = screen.getByLabelText('Choose time');
+    const guestsInput = screen.getByLabelText('Number of guests');
+    const occasionInput = screen.getByLabelText('Occasion');
+    const button = screen.getByRole('button', { name: 'Book a table' });
+
+    await user.type(firstNameInput, 'John');
+    await user.type(lastNameInput, 'Dee');
+    await user.type(dateInput, selectedDate);
+    await user.tab();
+    await user.selectOptions(timeInput, '18:00');
+    await user.clear(guestsInput);
+    await user.type(guestsInput, '2');
+    await user.selectOptions(occasionInput, 'Birthday');
+
+    expect(button).not.toBeDisabled();
+    await user.click(button);
+
+    await waitFor(() => {
+      expect(fetchAPI).toHaveBeenCalledWith(new Date(selectedDate));
+    });
+
+    await waitFor(() => {
+      expect(console.log).toHaveBeenCalledWith({
+        firstName: 'John',
+        lastName: 'Dee',
+        date: selectedDate,
+        time: '18:00',
+        guests: 2,
+        occasion: 'Birthday',
+      });
+    });
   });
 
   test('should check availableTimes state changes', async () => {
-    setupStubs();
+    fetchAPI.mockImplementation((date) => {
+      const dateStr = date.toISOString().substring(0, 10);
+      const schedule = {
+        '2023-06-20': ['18:00', '18:30'],
+        '2023-06-21': ['17:00', '18:30'],
+      };
+      return schedule[dateStr] || ['19:00'];
+    });
+
     render(<BookingPage />);
     const user = userEvent.setup();
 
@@ -58,25 +98,18 @@ describe('BookingPage', () => {
     await user.type(guestsInput, '2');
     await user.selectOptions(occasionInput, 'Birthday');
 
-    expect(button).not.toBeDisabled();
-
     await user.click(button);
-
-    await waitFor(() => {
-      expect(console.log).toBeCalledWith({
-        firstName: 'John',
-        lastName: 'Dee',
-        date: '2023-06-20',
-        time: '18:00',
-        guests: 2,
-        occasion: 'Birthday',
-      });
-    });
 
     await user.type(dateInput, '2023-06-21');
     await user.tab();
     await waitFor(() => {
       expect(screen.queryByRole('option', { name: '17:00' })).not.toBeNull();
+    });
+
+    await waitFor(() => {
+      expect(fetchAPI).toBeCalledTimes(3);
+      expect(fetchAPI).toHaveBeenNthCalledWith(2, new Date('2023-06-20'));
+      expect(fetchAPI).toHaveBeenNthCalledWith(3, new Date('2023-06-21'));
     });
   });
 });
