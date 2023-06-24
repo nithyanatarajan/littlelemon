8d560cb113cef3d6e9da5e4b21f2eae63d6efe17
Nithya Natarajan <nithyanatarajn@gmail.com>
2023-06-25 02:20:28 +0530

    Provide option to redirect on reaching coming soon page

--- src/pages/ComingSoonPage.js
+++ src/pages/ComingSoonPage.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Hero from '../components/Hero';
+import './ComingSoonPage.css';
 
 const ComingSoonPage = () => (
   <main>
     <Hero />
-    <p>Feature coming soon!</p>
+    <section className='comingsoon-section'>
+      <h2 className='comingsoon-subtitle'>Feature coming soon!</h2>
+      <NavLink to='/' className='primary-button'>
+        Go back to homepage
+      </NavLink>
+    </section>
   </main>
 );
 export default ComingSoonPage;
